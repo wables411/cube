fbb26795c7b50edb445f4d460b7cae344ba2b78f
Runitupronnie <runitupronnie@gmail.com>
2024-07-20 03:58:25 -0700

    Initial commit

---
+++ CUBE/script.js
@@ -0,0 +1,18 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const createEmoji = () => {
+        const emoji = document.createElement('div');
+        emoji.innerHTML = '💗';
+        emoji.classList.add('emoji');
+        emoji.style.left = Math.random() * 100 + 'vw';
+        emoji.style.animationDuration = Math.random() * 2 + 3 + 's';
+        document.body.appendChild(emoji);
+
+        setTimeout(() => {
+            emoji.remove();
+        }, 5000);
+    };
+
+    setInterval(createEmoji, 300);
+});
